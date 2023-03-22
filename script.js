81b591f828f3c8229c6b885c028305b0793e758a
Vítor Gabriel <vggd@cin.ufpe.br>
2023-03-22 12:14:06 -0300

    Otimização das funções com Inputs
    Organizei cada desafio para que eles possam ser realizados em qualquer ordem de acordo com a preferência do usuário.

---
+++ script.js
@@ -0,0 +1,67 @@
+// INVERSOR DE VARIÁVEIS
+function var_invert(){
+    //pega os dois elementos dados e os troca de posição
+    var a = document.getElementById('fnum').value
+    var b = document.getElementById('snum').value
+    var c = a // variavel auxiliar pra realziar o processo
+    a = b
+    b = c
+    // printa na barra abaixo o valor obtido
+    document.getElementById('result1').value = 'Variavel 1: ' + a +' | Variável 2: ' + b
+}
+
+// ALGORITMO DE FAIXA ETÁRIA
+function faixa_etaria(idade){
+    //compara com cada valor para cada faixa etária e printa na barra o valor obtido
+    if (idade >= 0 && idade < 15){
+        document.getElementById('result2').value = 'A sua faixa etária é de: Criança.'
+    }
+    else if (idade >= 15 && idade < 30){
+        document.getElementById('result2').value = 'A sua faixa etária é de: Jovem.'
+    }
+    else if (idade >= 30 && idade < 60){
+        document.getElementById('result2').value = 'A sua faixa etária é de: Adulto.'
+    }
+    else if (idade >= 60){
+        document.getElementById('result2').value = 'A sua faixa etária é de: Idoso.'
+    }
+    else{document.getElementById('result2').value = 'ERRO!'
+    }
+}
+// CALCULADORA DE IMC
+function imc_calc(){
+    //pega os valores de input de altura e peso
+    var altura = document.getElementById('altura').value
+    var peso = document.getElementById('peso').value
+    // transforma strings em valores float e a altura em metros
+    altura = (parseFloat(altura))/100
+    peso = parseFloat(peso)
+    // calculo do IMC
+    var m = peso / (altura ** 2)
+
+    // if e else com cada valor de cada IMC com suas descrições
+    if (m < 16){
+        document.getElementById('IMC').value = 'Seu IMC é: '+ Math.round(m) + ' | Baixo peso muito grave.'
+    }
+    else if(m >= 16 && m < 17){
+        document.getElementById('IMC').value = 'Seu IMC é: '+ Math.round(m) + ' | Baixo peso grave.'
+    }
+    else if(m >= 17 && m < 18.5){
+        document.getElementById('IMC').value = 'Seu IMC é: '+ Math.round(m) + ' | Baixo peso.'
+    }
+    else if(m >= 18.5 && m < 25){
+        document.getElementById('IMC').value = 'Seu IMC é: '+ Math.round(m) + ' | Peso Normal.'
+    }
+    else if(m >= 25 && m < 30){
+        document.getElementById('IMC').value = 'Seu IMC é: '+ Math.round(m) + ' | Sobrepeso.'
+    }
+    else if(m >= 30 && m < 35){
+        document.getElementById('IMC').value = 'Seu IMC é: '+ Math.round(m) + ' | Obesidade Grau I.'
+    }
+    else if(m >= 35 && m < 40){
+        document.getElementById('IMC').value = 'Seu IMC é: '+ Math.round(m) + ' | Obesidade Grau II.'
+    }
+    else{
+        document.getElementById('IMC').value = 'Seu IMC é: '+ Math.round(m) + ' | Obesidade Grau III.'
+    }
+}
--- var_invert.js
+++
@@ -1,9 +0,0 @@
-var a = prompt('Variavel 1')
-var b = prompt('Variável 2')
-var c = a
-a = b
-b = c
-
-
-document.write('O valor das variáveis foi invertido ')
-document.write('Variavel 1: ' + a +' | Variável 2: ' + b)
